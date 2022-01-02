154bebb7cd1c609145454942e237666520c1cf88
Andrew Ferguson <andrew@fergcorp.com>
2022-01-02 12:10:30 -0800

    Navigation updates

--- kanban_household_supply_card_generator/card/urls.py
+++ kanban_household_supply_card_generator/card/urls.py
@@ -7,13 +7,13 @@ from kanban_household_supply_card_generator.card.views import (
 
 app_name = "card"
 urlpatterns = [
-    path("card/", all_cards),
-    path("card/<int:pk>/", single_card),
+    path("card/", all_cards, name="all_cards"),
+    path("card/<int:pk>/", single_card, name="single_card"),
     path("card/<str:region>/<str:rack>/<str:shelf>/", all_cards),
     path("card/<str:region>/<str:rack>/", all_cards),
     path("card/<str:region>/", all_cards),
     path("shelf/<str:region>/<str:rack>/<str:shelf>/", all_shelf_labels),
     path("shelf/<str:region>/<str:rack>/", all_shelf_labels),
     path("shelf/<str:region>/", all_shelf_labels),
-    path("shelf/", all_shelf_labels),
+    path("shelf/", all_shelf_labels, name="all_labels"),
 ]
